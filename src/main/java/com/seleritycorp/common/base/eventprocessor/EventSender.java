559a00257039f3deebeb2daf01a9cef53cc1aba7
Christian Aistleitner <christian.aistleitner@selerityinc.com>
2017-05-10 15:21:11 +0200

    Add helper for sending Event Processor events
    This helper takes automatically performs the required decoration of
    the event's payload.

---
+++ src/main/java/com/seleritycorp/common/base/eventprocessor/EventSender.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (C) 2016-2017 Selerity, Inc. (support@seleritycorp.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.seleritycorp.common.base.eventprocessor;
+
+import com.google.gson.JsonObject;
+
+import com.seleritycorp.common.base.config.ApplicationConfig;
+import com.seleritycorp.common.base.config.Config;
+import com.seleritycorp.common.base.http.client.HttpException;
+import com.seleritycorp.common.base.http.client.HttpRequestFactory;
+import com.seleritycorp.common.base.logging.Log;
+import com.seleritycorp.common.base.logging.LogFactory;
+import com.seleritycorp.common.base.uuid.UuidGenerator;
+
+import org.apache.http.HttpStatus;
+
+import java.util.UUID;
+
+import javax.inject.Inject;
+
+/**
+ * Helper to send Events to Event Processor.
+ */
+public class EventSender {
+  private static final Log log = LogFactory.getLog(EventSender.class);
+
+  private final UuidGenerator uuidGenerator;
+  private final String targetUrl;
+  private final HttpRequestFactory requestFactory;
+
+  /**
+   * Creates a Sender for EventProcessing events.
+   * 
+   * @param config The application config to use.
+   * @param uuidGenerator Used for request ids.
+   * @param requestFactory Factory for Http requests.
+   */
+  @Inject
+  public EventSender(@ApplicationConfig Config config, UuidGenerator uuidGenerator,
+      HttpRequestFactory requestFactory) {
+    this.uuidGenerator = uuidGenerator;
+    this.targetUrl = config.get("EventProcessor.beaconUrl",
+        "https://event.seleritycorp.com/beacon");
+    this.requestFactory = requestFactory;
+  }
+
+  /**
+   * Send an Event to EventProcessor while ignoring eventual issues.
+   *
+   * <p>If there are issues with sending, they'll be logged, but they won't be escalated to the
+   * caller.
+   *
+   * <p>Use this method for fire-and-forget events. If you want to check that atleast that the
+   * Http aspect of sending worked, see {@link #sendChecked(String, int, JsonObject)} instead.
+   *
+   * @param schema The name of the schema to send an event for
+   * @param schemaVersion The version number for the schema
+   * @param payload The payload to send.
+   * @return The UUID of the sent event.
+   */
+  public UUID send(String schema, int schemaVersion, JsonObject payload) {
+    UUID uuid = uuidGenerator.generate();
+    try {
+      sendChecked(uuid, schema, schemaVersion, payload);
+    } catch (Exception e) {
+      log.info("Failed to send beacon for " + schema + " (version " + schemaVersion + ") to "
+          + targetUrl, e);
+    }
+    return uuid;
+  }
+
+  /**
+   * Send an Event to EventProcessor and escalates errors to the caller.
+   *
+   * <p>If no exceptions get thrown, this does not mean that the sent event passed validation.
+   * It only means that on the HTTP layer everything worked. Event Processor is responding
+   * before event validation takes place. So you need to check on the Event Processor side to
+   * see if validation passes.
+   *
+   * <p>A fire-and-forget variant of sending events can be found at
+   * {@link #send(String, int, JsonObject)}.
+   *   
+   * @param schema The name of the schema to send an event for
+   * @param schemaVersion The version number for the schema
+   * @param payload The payload to send.
+   * @return The UUID of the sent event.
+   * @throws HttpException if there are errors sending the request.
+   */
+  public UUID sendChecked(String schema, int schemaVersion, JsonObject payload)
+      throws HttpException {
+    UUID uuid = uuidGenerator.generate();
+    sendChecked(uuid, schema, schemaVersion, payload);
+    return uuid;
+  }
+
+  /**
+   * Send an Event to EventProcessor and escalates errors to the caller.
+   *
+   * <p>If no exceptions get thrown, this does not mean that the sent event passed validation.
+   * It only means that on the HTTP layer everything worked. Event Processor is responding
+   * before event validation takes place. So you need to check on the Event Processor side to
+   * see if validation passes.
+   *   
+   * @param schema The name of the schema to send an event for
+   * @param schemaVersion The version number for the schema
+   * @param payload The payload to send.
+   * @throws HttpException if there are errors sending the request.
+   */
+  private void sendChecked(UUID uuid, String schema, int schemaVersion, JsonObject payload)
+      throws HttpException {
+    JsonObject data = new JsonObject();
+    data.addProperty("schema", schema);
+    data.addProperty("schemaVersion", schemaVersion);
+    data.addProperty("uuid", uuid.toString());
+    data.add("payload", payload);
+    requestFactory.createPostJson(targetUrl, data)
+      .setExpectedStatusCode(HttpStatus.SC_NO_CONTENT)
+      .execute();
+  }
+}
---
+++ src/test/java/com/seleritycorp/common/base/eventprocessor/EventSenderTest.java
@@ -0,0 +1,175 @@
+/*
+ * Copyright (C) 2016-2017 Selerity, Inc. (support@seleritycorp.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.seleritycorp.common.base.eventprocessor;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.failBecauseExceptionWasNotThrown;
+
+import static org.easymock.EasyMock.capture;
+import static org.easymock.EasyMock.eq;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.newCapture;
+
+import java.util.UUID;
+
+import org.easymock.Capture;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.gson.JsonObject;
+import com.seleritycorp.common.base.http.client.HttpException;
+import com.seleritycorp.common.base.http.client.HttpRequest;
+import com.seleritycorp.common.base.http.client.HttpRequestFactory;
+import com.seleritycorp.common.base.test.InjectingTestCase;
+import com.seleritycorp.common.base.test.SettableConfig;
+import com.seleritycorp.common.base.uuid.UuidGenerator;
+
+public class EventSenderTest extends InjectingTestCase {
+  private SettableConfig config;
+  private UuidGenerator uuidGenerator;
+  private HttpRequestFactory httpRequestFactory;
+  
+  @Before
+  public void setUp() {
+    this.config = new SettableConfig();
+    config.set("EventProcessor.beaconUrl", "foo://example.org/beacon");
+    this.uuidGenerator = getUuidGenerator();
+    this.httpRequestFactory = createMock(HttpRequestFactory.class);
+  }
+
+  @Test
+  public void testSendOk() throws HttpException {
+    JsonObject payload = new JsonObject();
+    Capture<JsonObject> jsonCapture = newCapture();
+
+    HttpRequest request1 = createMock(HttpRequest.class);
+    HttpRequest request2 = createMock(HttpRequest.class);
+    
+    expect(httpRequestFactory.createPostJson(eq("foo://example.org/beacon"), capture(jsonCapture)))
+    .andReturn(request1);
+    expect(request1.setExpectedStatusCode(204)).andReturn(request2);
+    expect(request2.execute()).andReturn(null);
+    
+    replayAll();
+
+    EventSender sender = createEventSender();    
+    UUID uuid = sender.send("foo", 42, payload);
+    
+    verifyAll();
+
+    assertThat(uuid.toString()).isEqualTo("00000000-0000-0000-0000-000000000001");
+
+    JsonObject fullSentObject = jsonCapture.getValue();
+    assertThat(fullSentObject.get("schema").getAsString()).isEqualTo("foo");
+    assertThat(fullSentObject.get("schemaVersion").getAsNumber()).isEqualTo(42);
+    assertThat(fullSentObject.get("uuid").getAsString()).isEqualTo("00000000-0000-0000-0000-000000000001");
+    assertThat(fullSentObject.get("payload")).isEqualTo(payload);
+  }
+
+  @Test
+  public void testSendException() throws HttpException {
+    RuntimeException exception = new RuntimeException("catch me"); 
+    JsonObject payload = new JsonObject();
+    Capture<JsonObject> jsonCapture = newCapture();
+
+    
+    expect(httpRequestFactory.createPostJson(eq("foo://example.org/beacon"), capture(jsonCapture)))
+      .andThrow(exception);
+    
+    replayAll();
+
+    EventSender sender = createEventSender();    
+    UUID uuid = sender.send("foo", 42, payload);
+    
+    verifyAll();
+
+    assertThat(uuid.toString()).isEqualTo("00000000-0000-0000-0000-000000000001");
+
+    JsonObject fullSentObject = jsonCapture.getValue();
+    assertThat(fullSentObject.get("schema").getAsString()).isEqualTo("foo");
+    assertThat(fullSentObject.get("schemaVersion").getAsNumber()).isEqualTo(42);
+    assertThat(fullSentObject.get("uuid").getAsString()).isEqualTo("00000000-0000-0000-0000-000000000001");
+    assertThat(fullSentObject.get("payload")).isEqualTo(payload);
+  }
+
+  @Test
+  public void testSendChecked() throws HttpException {
+    JsonObject payload = new JsonObject();
+    Capture<JsonObject> jsonCapture = newCapture();
+
+    HttpRequest request1 = createMock(HttpRequest.class);
+    HttpRequest request2 = createMock(HttpRequest.class);
+    
+    expect(httpRequestFactory.createPostJson(eq("foo://example.org/beacon"), capture(jsonCapture)))
+    .andReturn(request1);
+    expect(request1.setExpectedStatusCode(204)).andReturn(request2);
+    expect(request2.execute()).andReturn(null);
+    
+    replayAll();
+
+    EventSender sender = createEventSender();    
+    UUID uuid = sender.sendChecked("foo", 42, payload);
+    
+    verifyAll();
+
+    assertThat(uuid.toString()).isEqualTo("00000000-0000-0000-0000-000000000001");
+
+    JsonObject fullSentObject = jsonCapture.getValue();
+    assertThat(fullSentObject.get("schema").getAsString()).isEqualTo("foo");
+    assertThat(fullSentObject.get("schemaVersion").getAsNumber()).isEqualTo(42);
+    assertThat(fullSentObject.get("uuid").getAsString()).isEqualTo("00000000-0000-0000-0000-000000000001");
+    assertThat(fullSentObject.get("payload")).isEqualTo(payload);
+  }
+
+  @Test
+  public void testSendCheckedException() throws HttpException {
+    HttpException exception = new HttpException("catch me"); 
+    JsonObject payload = new JsonObject();
+    Capture<JsonObject> jsonCapture = newCapture();
+
+    
+    HttpRequest request1 = createMock(HttpRequest.class);
+    HttpRequest request2 = createMock(HttpRequest.class);
+    
+    expect(httpRequestFactory.createPostJson(eq("foo://example.org/beacon"), capture(jsonCapture)))
+    .andReturn(request1);
+    expect(request1.setExpectedStatusCode(204)).andReturn(request2);
+    expect(request2.execute()).andThrow(exception);
+    
+    replayAll();
+
+    EventSender sender = createEventSender();    
+    try {
+      sender.sendChecked("foo", 42, payload);
+      failBecauseExceptionWasNotThrown(HttpException.class);
+    } catch (HttpException e) {
+      assertThat(e).isSameAs(exception);
+    }
+    
+    verifyAll();
+
+    JsonObject fullSentObject = jsonCapture.getValue();
+    assertThat(fullSentObject.get("schema").getAsString()).isEqualTo("foo");
+    assertThat(fullSentObject.get("schemaVersion").getAsNumber()).isEqualTo(42);
+    assertThat(fullSentObject.get("uuid").getAsString()).isEqualTo("00000000-0000-0000-0000-000000000001");
+    assertThat(fullSentObject.get("payload")).isEqualTo(payload);
+  }
+
+  private EventSender createEventSender() {
+    return new EventSender(config, uuidGenerator, httpRequestFactory);
+  }
+}
